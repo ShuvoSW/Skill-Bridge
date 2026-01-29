2eabbcf1a734c4526351ecea73d6bf5aa276fd01
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-29 13:28:22 +0600

    Pages & Routes add

---
+++ frontend/src/app/tutor/availability/page.tsx
@@ -0,0 +1,7 @@
+export default function Availability() {
+  return (
+    <div>
+      <h1>This is blogs page </h1>
+    </div>
+  );
+}
---
+++ frontend/src/app/tutor/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function TutorProfile() {
+  return (
+    <div>
+      <h1>This is blogs page </h1>
+    </div>
+  );
+}
