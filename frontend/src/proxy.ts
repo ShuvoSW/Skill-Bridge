bf0df8d0ab682609be81b7708944d1c0ba574deb
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-31 02:06:09 +0600

    Proxy

--- frontend/src/proxy.ts
+++ frontend/src/proxy.ts
@@ -1,30 +1,31 @@
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { getCurrentUser } from "./services/auth";
 // import { getCurrentUser } from "./services/auth/auth";
-// // import { getCurrentUser } from "./services/user.service";
+// import { getCurrentUser } from "./services/user.service";
 
-// const ALLOWED_ROLES = ["user", "admin"];
-// const PUBLIC_ROUTES = ["/login", "/register"];
+const ALLOWED_ROLES = ["user", "admin"];
+const PUBLIC_ROUTES = ["/login", "/register"];
 
-// export async function proxy(request: NextRequest) {
-//   const { pathname , origin } = request.nextUrl;
+export async function proxy(request: NextRequest) {
+  const { pathname , origin } = request.nextUrl;
 
-//   if (PUBLIC_ROUTES.includes(pathname)) {
-//     return NextResponse.next();
-//   }
+  if (PUBLIC_ROUTES.includes(pathname)) {
+    return NextResponse.next();
+  }
 
-//   const userInfo = await getCurrentUser();
+  const userInfo = await getCurrentUser();
 
-//   if (!userInfo) {
-//     return NextResponse.redirect(new URL(`/login?redirectPath=${pathname}`, origin));
-//   }
+  if (!userInfo) {
+    return NextResponse.redirect(new URL(`/login?redirectPath=${pathname}`, origin));
+  }
 
-//   if (!ALLOWED_ROLES.includes(userInfo.role)) {
-//     return NextResponse.redirect(new URL(`/login?redirectPath=${pathname}`, origin));
-//   }
+  if (!ALLOWED_ROLES.includes(userInfo.role)) {
+    return NextResponse.redirect(new URL(`/login?redirectPath=${pathname}`, origin));
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: ["/dashboard", "/dashboard/:path*"],
-// };
+export const config = {
+  matcher: ["/dashboard", "/dashboard/:path*"],
+};
