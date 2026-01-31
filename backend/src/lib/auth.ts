328a26b8e5107fbb40525ea2f74aaa9b7cec1970
ShuvoSW <shuvomajumder3369@gmail.com>
2026-01-31 21:44:54 +0600

    Add nodemailer and email verification

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -1,6 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.APP_USER,
+    pass: process.env.APP_PASS,
+  },
+});
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -9,9 +20,42 @@ export const auth = betterAuth({
 
     trustedOrigins: [process.env.APP_URL!],
 
+    session: {
+    cookie: {
+      name: "better-auth.session_token",
+      sameSite: "lax",
+      path: "/",
+      httpOnly: true,
+      secure: false,
+    },
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "STUDENT",
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      status: {
+        type: "string",
+        defaultValue: "ACTIVE",
+        required: false,
+      },
+    },
+  },
+
     emailAndPassword: {
         enabled: true,
+        autoSignIn: false,
+    requireEmailVerification: false,
     },
     
-    
+     emailVerification: {
+    sendOnSignUp: false, 
+    autoSignInAfterVerification: true,
+  },
 });
